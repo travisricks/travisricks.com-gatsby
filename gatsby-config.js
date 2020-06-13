72287ba1069ca16996ec292e3d6463a89c9cd27e
Travis Ricks <travis@travisricks.com>
2020-06-13 21:37:26 +0900

    add react-helmet for metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  plugins: [`gatsby-plugin-postcss`],
+  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-react-helmet"],
+  siteMetadata: {
+    title: "Travis Ricks - Fullstack Software Engineer",
+    description:
+      "Travis Ricks is a Tokyo-based fullstack engineer that specializes in building UIs with JavaScript",
+  },
 };
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import Header from "./header";
 import "../styles/styles.css";
+import Helmet from "react-helmet";
+import useSiteMetadata from "../hooks/useSiteMetadata";
 
-const Layout = ({ children }) => (
-  <>
-    <Header />
-    <main className="container mx-auto">{children}</main>
-  </>
-);
+const Layout = ({ children }) => {
+  const { title, description } = useSiteMetadata();
+  return (
+    <>
+      <Helmet>
+        <html lang="en" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Helmet>
+      <Header />
+      <main className="container mx-auto">{children}</main>
+    </>
+  );
+};
 
 export default Layout;
